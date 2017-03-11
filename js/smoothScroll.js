c2caf793a65647ef668748cdfafcf128605a0749
Wanjugu Irene <ireneemaina@gmail.com>
2017-03-11 18:16:01 +0300

    Slight Changes

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -25,7 +25,24 @@ function scrollNav() {
     $('.'+theClass).parent('a').addClass('active');
     //Animate
     $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 50
+        scrollTop: $( $(this).attr('href') ).offset().top - 70
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+function scrollNav() {
+  $('.mobNav a').click(function(){
+    //Toggle Class
+    $(".active").removeClass("active");
+    $(this).closest('a').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('a').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 60
     }, 400);
     return false;
   });
